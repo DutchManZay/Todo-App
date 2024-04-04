16531501d69cdfea85db55a9406f2ce5a7e8f6cc
Olayinka Ajibola <rhozario1650@gmail.com>
2024-04-04 21:54:32 +0000

    first update

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const inputbox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addtask(){
+function addTask(){
     if(inputbox.value === ''){
         alert("You must write something!");
     }
     else{
         let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        li.innerHTML = inputbox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
